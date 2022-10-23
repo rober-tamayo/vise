d582860dd4abf158f793557d2f50d951bbef81a7
Roberto <rocart.89@gmail.com>
2022-10-23 09:19:07 -0500

    nuevo commit

--- src/main/java/net/codejava/ProductRepository.java
+++ src/main/java/net/codejava/ProductRepository.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
 	List<Product> findAllByBrand(String brand);
+	
 	List<Product> findByPriceGreaterThanEqual(float price);
+	
 }
